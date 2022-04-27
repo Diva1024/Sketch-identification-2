417c7996b1ffc6164d2513fcb7c5d467aa6717c5
Diva1024 <80517970+Diva1024@users.noreply.github.com>
2022-04-27 15:19:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+
+}
+function setup(){
+canvas=createCanvas(280,280);
+canvas.center();
+background("white");
+}
+function draw(){
+
+}
+function clearCanvas(){
+    background("white");
+
+}
